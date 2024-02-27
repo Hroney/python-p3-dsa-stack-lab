98ae6acb44dbf4ac55d8e20aa02177faa4fda871
Hayden Roney <roneyhayden@gmail.com>
2024-02-27 10:41:38 -0600

    finishes lab

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,25 +1,46 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
+        self.items = items
+        self.limit = limit
         pass
 
     def isEmpty(self):
-        pass
+        if self.size() == 0:
+            return True
+        else:
+            return False
 
     def push(self, item):
+        if self.size() == self.limit:
+            pass
+        else:
+            self.items.append(item)
         pass
 
     def pop(self):
-        pass
+        if len(self.items) == 0:
+            return None
+        else:
+            return self.items.pop()
 
     def peek(self):
         pass
     
     def size(self):
-        pass
+        return len(self.items)
 
     def full(self):
-        pass
+        if len(self.items) == self.limit:
+            return True
+        else:
+            return False
 
     def search(self, target):
-        pass
+        depth = self.size() - 1
+        for value in self.items:
+            if value == target:
+                return depth
+            else:
+                depth -= 1
+        return -1
